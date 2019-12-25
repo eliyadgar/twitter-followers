4041b081b4e19503bfea3cb1e024b68db5e91533
eliyadgar <eliyadgar@gmail.com>
2019-12-25 14:42:33 +0200

    create search bar and search handler

--- twitter-followers/src/App.js
+++ twitter-followers/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import Bar from './components/Bar'
 import './App.css';
+import debounce from 'lodash/debounce'
+import {getFollowers} from './twitterAPI'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  handleSearch = debounce(async (value) => {
+    if (value.length > 3) {
+        const followers = await getFollowers(value)
+        console.log('@@@@@', followers);
+        
+    }
+  }, 1000)
+  
+
+  render() {
+    return (
+      <div className="App">
+        <Bar handleSearch={this.handleSearch}/>
+      </div>
+    );
+  }
 }
 
 export default App;
